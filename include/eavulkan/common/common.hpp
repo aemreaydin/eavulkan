d49babf2f305770921cecd85bd3be53c0e06e1d7
Emre AYDIN <aeaydin1@gmail.com>
2023-09-02 19:16:41 -0400

    Common and includes added.

--- include/eavulkan/common/common.hpp
+++ include/eavulkan/common/common.hpp
@@ -1,57 +1,38 @@
-#ifndef common_h
-#define common_h
+#ifndef EAVULKAN_COMMON_COMMON_HPP
+#define EAVULKAN_COMMON_COMMON_HPP
 
-#include <SDL.h>
-#include <SDL_vulkan.h>
-#include <gsl/gsl>
-#include <spdlog/spdlog.h>
-#include <tiny_obj_loader.h>
-#include <vulkan/vulkan.h>
+#include <eavulkan/common/includes.hpp>
 
-#include <array>
-#include <filesystem>
-#include <fstream>
-#include <functional>
-#include <iostream>
-#include <map>
-#include <set>
-#include <span>
-#include <vector>
-
-template <class T> constexpr void VK_CHECK( T func, const char* err )
-{
-    VkResult res = func;
-    if ( res != VK_SUCCESS )
-    {
-        throw std::runtime_error( fmt::format( "{}: {}", err, static_cast<uint32_t>( res ) ) );
-    }
+template <class T>
+constexpr void vkCheck(T func, const char* err) {
+  VkResult res = func;
+  if (res != VK_SUCCESS) {
+    throw std::runtime_error(fmt::format("{}: {}", err, static_cast<uint32_t>(res)));
+  }
 }
 
-template <class T> constexpr uint32_t TO_U32( T res )
-{
-    return static_cast<uint32_t>( res );
+template <class T>
+constexpr auto toU32(T res) -> uint32_t {
+  return static_cast<uint32_t>(res);
 }
 
 // Todo(emreaydn) refactor to read from any file and add one to read shader files
-template <typename T> std::vector<T> readFromFile( const std::string& shaderPath )
-{
-    std::ifstream ifs{ shaderPath, std::ios::binary | std::ios::ate };
-    if ( !ifs )
-    {
-        throw std::runtime_error( fmt::format( "Failed to open shader file from {}", shaderPath ) );
-    }
-    const auto size = ifs.tellg();
-    ifs.seekg( 0, std::ios::beg );
+template <typename T>
+auto readShaderFile(const std::string& shader_path) -> std::vector<T> {
+  std::ifstream ifs{shader_path, std::ios::binary | std::ios::ate};
+  if (!ifs) {
+    throw std::runtime_error(fmt::format("Failed to open shader file from {}", shader_path));
+  }
+  const auto size = ifs.tellg();
+  ifs.seekg(0, std::ios::beg);
 
-    std::vector<T> buffer( size );
+  std::vector<T> buffer(size);
 
-    if ( !ifs.read( reinterpret_cast<char*>( buffer.data() ),
-                    static_cast<uint32_t>( buffer.size() ) ) )
-    {
-        throw std::runtime_error( fmt::format( "Failed to read shader file from {}", shaderPath ) );
-    }
+  if (!ifs.read(buffer.data(), TO_U32(buffer.size()))) {
+    throw std::runtime_error(fmt::format("Failed to read shader file from {}", shader_path));
+  }
 
-    return buffer;
+  return buffer;
 }
 
 #endif
---
+++ include/eavulkan/common/includes.hpp
@@ -0,0 +1,21 @@
+#ifndef EAVULKAN_COMMON_INCLUDES_HPP
+#define EAVULKAN_COMMON_INCLUDES_HPP
+
+#include <SDL.h>
+#include <SDL_vulkan.h>
+#include <spdlog/spdlog.h>
+#include <tiny_obj_loader.h>
+#include <vulkan/vulkan.h>
+
+#include <array>
+#include <filesystem>
+#include <fstream>
+#include <functional>
+#include <gsl/gsl>
+#include <iostream>
+#include <map>
+#include <set>
+#include <span>
+#include <vector>
+
+#endif

3daf0cafcc302625f37f411aad5985d3e0a06ab0
Emre AYDIN <aeaydin1@gmail.com>
2023-09-02 19:56:45 -0400

    Physical Device added.

---
+++ include/eavulkan/physical_device.hpp
@@ -0,0 +1,33 @@
+#ifndef V_PHYSICAL_DEVICE_H
+#define V_PHYSICAL_DEVICE_H
+
+#include <eavulkan/common/includes.hpp>
+
+namespace rendering {
+
+struct PhysicalDeviceProperties {
+  VkPhysicalDeviceProperties physicalDeviceProperties{};
+  VkPhysicalDeviceFeatures physicalDeviceFeatures{};
+  std::vector<VkQueueFamilyProperties> queueFamilyProperties{};
+  VkPhysicalDeviceMemoryProperties memoryProperties{};
+
+  explicit PhysicalDeviceProperties(VkPhysicalDevice physical_device);
+};
+
+class Instance;
+class PhysicalDevice {
+  VkPhysicalDevice _physicalDevice{nullptr};
+  PhysicalDeviceProperties _physicalDeviceProperties;
+
+  static auto FindSuitablePhysicalDevice(VkInstance instance, VkSurfaceKHR surface) -> VkPhysicalDevice;
+
+ public:
+  explicit PhysicalDevice(VkInstance instance, VkSurfaceKHR surface);
+
+  [[nodiscard]] auto GetHandle() const -> VkPhysicalDevice;
+  [[nodiscard]] auto GetPhysicalDeviceProperties() const -> const PhysicalDeviceProperties&;
+};
+
+}  // namespace rendering
+
+#endif
---
+++ src/physical_device.cpp
@@ -0,0 +1,55 @@
+#include <eavulkan/common/common.hpp>
+#include <eavulkan/physical_device.hpp>
+
+namespace rendering {
+
+PhysicalDeviceProperties::PhysicalDeviceProperties(VkPhysicalDevice physical_device) {
+  vkGetPhysicalDeviceProperties(physical_device, &physicalDeviceProperties);
+
+  uint32_t queueFamilyCount{};
+  vkGetPhysicalDeviceQueueFamilyProperties(physical_device, &queueFamilyCount, nullptr);
+  queueFamilyProperties.resize(queueFamilyCount);
+  vkGetPhysicalDeviceQueueFamilyProperties(physical_device, &queueFamilyCount, queueFamilyProperties.data());
+
+  vkGetPhysicalDeviceFeatures(physical_device, &physicalDeviceFeatures);
+  vkGetPhysicalDeviceMemoryProperties(physical_device, &memoryProperties);
+}
+
+PhysicalDevice::PhysicalDevice(VkInstance instance, VkSurfaceKHR surface)
+    : _physicalDevice(FindSuitablePhysicalDevice(instance, surface)), _physicalDeviceProperties(_physicalDevice) {}
+
+auto PhysicalDevice::FindSuitablePhysicalDevice(VkInstance instance, VkSurfaceKHR surface) -> VkPhysicalDevice {
+  uint32_t physicalDeviceCount{};
+  vkEnumeratePhysicalDevices(instance, &physicalDeviceCount, nullptr);
+  std::vector<VkPhysicalDevice> availableDevices(physicalDeviceCount);
+  vkEnumeratePhysicalDevices(instance, &physicalDeviceCount, availableDevices.data());
+
+  if (availableDevices.empty()) {
+    throw std::runtime_error("No physical devices found on the system.");
+  }
+
+  for (const auto& physicalDevice : availableDevices) {
+    const auto physicalDeviceProperties = PhysicalDeviceProperties(physicalDevice);
+    if (physicalDeviceProperties.physicalDeviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU &&
+        surface != nullptr) {
+      for (uint32_t idx = 0; idx != physicalDeviceProperties.queueFamilyProperties.size(); ++idx) {
+        VkBool32 surfaceSupported{VK_FALSE};
+        vkCheck(vkGetPhysicalDeviceSurfaceSupportKHR(physicalDevice, idx, surface, &surfaceSupported),
+                "Failed to get physical device surface support.");
+        if (surfaceSupported == VK_TRUE) {
+          return physicalDevice;
+        }
+      }
+    }
+  }
+
+  return availableDevices[0];
+}
+
+auto PhysicalDevice::GetHandle() const -> VkPhysicalDevice { return _physicalDevice; }
+
+auto PhysicalDevice::GetPhysicalDeviceProperties() const -> const PhysicalDeviceProperties& {
+  return _physicalDeviceProperties;
+}
+
+}  // namespace rendering
